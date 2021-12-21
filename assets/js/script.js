890fc0cf429b8153c3337f9e7cdc1fa53285ac2a
Jonathan F-Carait <jonathan.carait@outlook.com>
2021-12-21 21:41:12 +1100

    updated array with questions, choices and answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,44 @@
-questions = [
+var questions = [
     {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
+        question: "Which type of JavaScript language is ___",
+        choices: [ "Object-Oriented", "Object-Based", "Assembly-language", "High-level" ],
+        answer: "High-level"
     }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
+        question: "Which one of the following also known as Conditional Expression:",
+        choices: [ "Alternative to if-else", "Switch statement", "If-then-else statement", "immediate if" ],
+        answer: "immediate if"
     }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
+        question: "In JavaScript, what is a block of statement?",
+        choices: [ "Conditional block", "block that combines a number of statements into a single compound statement", "both conditional block and a single statement", "block that contains a single statement" ],
+        answer: "block that combines a number of statements into a single compound statement"
     }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
+        question: "When interpreter encounters an empty statements, what it will do:",
+        choices: [ "Shows a warning", "Prompts to complete the statement", "Throws an error", "Ignores the statements" ],
+        answer: "Ignores the statements"
     }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
+        question: "The 'function' and 'var' are known as:",
+        choices: [ "Keywords", "Data types", "Declaration statements", "Prototypes" ],
+        answer: "Declaration statements"
     }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
+        question: "Which of the following variables takes precedence over the others if the names are the same?",
+        choices: [ "Global variable", "The local element", "The two of the above", "None of the above" ],
+        answer: "The local element"
     }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
+        question: "Which one of the following is the correct way for calling the JavaScript code?",
+        choices: [ "Preprocessor", "Triggering Event", "RMI", "Function/Method" ],
+        answer: "Function/Method"
     }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
+        question: "In the JavaScript, which one of the following is not considered as an error:",
+        choices: [ "Syntax error", "Missing of semicolons", "Division by zero", "Missing of Bracket" ],
+        answer: "Division by zero"
     }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
+        question: "Which of the following given functions of the Number Object formats a number with a different number of digits to the right of the decimal?",
+        choices: [ "toExponential()", "toFixed()", "toPrecision()", "toLocaleString()" ],
+        answer: "toPrecision()"
     }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
-    }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
-    }, {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
+        question: "Which of the following function of the String object returns the character in the string starting at the specified position via the specified number of characters?",
+        choices: [ "slice()", "split()", "substr()", "search()" ],
+        answer: "substr()"
     }]
 
 
