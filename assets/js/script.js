98e69067f2a02532a11e282ccf7d96a5b5be8e1c
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2021-12-23 09:32:23 +1100

    added quiz completion function, working on fix for countdown so that it stops when quiz is finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //All html element variables
 var introContainer = document.getElementsByClassName("intro-container");
 var wrapper = document.getElementsByClassName("wrapper");
+var completeEl = document.getElementById("quiz-done");
 var highScores = document.getElementById("high-scores")
 var countdownDisplay = document.getElementById("countdown");
 var timerEl = document.getElementById("timer");
@@ -19,6 +20,7 @@ var delay = 3000;
 
 
 var questionIndex = 0;
+var isGameOver = false;
 
 // clears the intro at beginning of quiz
 function clearIntro() {
@@ -33,20 +35,20 @@ function renderQnA() {
 
     question.textContent = "";
     multipleChoice.innerHTML = "";
-    
+
     var currentQuestion = questions[questionIndex];
 
-    
+
     question.textContent = currentQuestion.question;
- 
+
 
     var questionsArray = currentQuestion.choices;
-    
-    for (var i = 0; i < questionsArray.length; i++ ) {
+
+    for (var i = 0; i < questionsArray.length; i++) {
         var choice = document.createElement("li");
         choice.innerHTML = questionsArray[i];
-        multipleChoice.appendChild(choice); 
-    }  
+        multipleChoice.appendChild(choice);
+    }
     multipleChoice.addEventListener("click", (checkAnswer));
 }
 
@@ -54,41 +56,89 @@ function renderQnA() {
 //checks for answer and generates next question
 function checkAnswer(event) {
     var element = event.target;
-    
+
 
     if (element.matches("li")) {
 
-    if (questions[questionIndex].answer == element.textContent) {
-        feedback.textContent = "Correct!";
-        score = score+100;
-    } else {
-        feedback.textContent = "Wrong!";
-        timeLeft = timeLeft-5;
+        if (questions[questionIndex].answer == element.textContent) {
+            feedback.textContent = "Correct!";
+            score = score + 100;
+        } else {
+            feedback.textContent = "Wrong!";
+            timeLeft = timeLeft - 5;
+        }
     }
-  }
-  questionIndex++;
-  renderQnA();
-  console.log(score);
-  
 
-  setTimeout(function () {
-      feedback.textContent = "";
-  }, 750)
+    if (questionIndex === questions.length-1) {
+        quizDone();
 
+        setTimeout(function () {
+            feedback.textContent = "";
+        }, 750)
+    } else {
+        questionIndex++;
+        renderQnA();
+        console.log(score);
+
+
+        setTimeout(function () {
+            feedback.textContent = "";
+        }, 750)
+    }
 };
 
-function countdown () {
+
+// game timer
+function countdown() {
     timeLeft = 60;
 
     var timeInterval = setInterval(function () {
+
+        
+
         if (timeLeft > 0) {
             timerEl.textContent = timeLeft;
             timeLeft--;
+        } else if (isGameOver && timeLeft > 0 ) {
+            clearInterval(timeInterval);
+            timerEl.textContent = 0;
         } else {
             clearInterval(timeInterval);
+            timerEl.textContent = 0;
+            quizDone();
         }
     }, 1000);
 }
+
+function quizDone() {
+    question.textContent = "";
+    multipleChoice.innerHTML = "";
+
+    var quizCompleted = document.createElement("p");
+    var totalScore = document.createElement("p");
+    var formEl = document.createElement("form");
+    var formLabel = document.createElement("label")
+    var formInput = document.createElement("input")
+    var buttonEl = document.createElement("button")
+
+    quizCompleted.textContent = "All done!";
+    totalScore.textContent = "Your score is: " + score;
+    formLabel.textContent = "Enter your initials: ";
+    buttonEl.textContent = "Submit";
+    
+    formInput.setAttribute("type", "text");
+    buttonEl.setAttribute("type", "button");
+
+
+    completeEl.appendChild(quizCompleted);
+    completeEl.appendChild(totalScore);
+    completeEl.appendChild(formEl);
+    formEl.appendChild(formLabel);
+    formEl.appendChild(formInput);
+    formEl.appendChild(buttonEl);
+};
+
+
 
 
 
@@ -99,8 +149,8 @@ startBtn.addEventListener("click", function () {
     clearIntro();
     renderQnA();
     countdown();
-    // initQuiz();
 });
+
 
 
 
