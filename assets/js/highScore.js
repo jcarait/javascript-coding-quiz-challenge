5b751ef9135ac6ab98260f97752d1e0adc3af76d
Jonathan F-Carait <jonathan.carait@outlook.com>
2021-12-23 17:04:30 +1100

    working on highscores to display correctly

--- assets/js/highScore.js
+++ assets/js/highScore.js
@@ -1,27 +1,46 @@
 var backBtn = document.getElementById("back");
+var clearBtn = document.getElementById("clear");
 var displayhighScores = document.getElementById("display-high-scores")
 
 backBtn.addEventListener("click", function () {
     window.location.replace("./index.html");
 })
 
-var playerData = [];   
+var playerData = [];
 
+var storedHighScores = JSON.parse(localStorage.getItem("highScores"))
+
+if (storedHighScores !== null) {
     var player = JSON.parse(localStorage.getItem("player"));
 
     playerData.push(player);
 
- 
+
 
     console.log(playerData.length);
 
+displayScores(playerData)
+localStorage.setItem("highScores", JSON.stringify(playerData));
+    
+} else {
+    displayScores(storedHighScores);
+    saveHighScores();
+}
 
-    for (var i = 0; i < playerData.length; i++ ) {
-       var playerList = document.createElement("li")
-    playerList.textContent = playerData[i].initials + " - " + playerData[i].score; 
-    displayhighScores.appendChild(playerList);
+
+
+function saveHighScores() {
+    
+    localStorage.setItem("highScores", JSON.stringify(storedHighScores));
+};
+
+function displayScores(storedData) {
+    for (var i = 0; i < storedData.length; i++) {
+        var playerList = document.createElement("li")
+        playerList.textContent = storedData[i].initials + " - " + storedData[i].score;
+        displayhighScores.appendChild(playerList);
     }
-
+}
 
 
 
